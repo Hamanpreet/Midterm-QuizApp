eb9749efa52480d072428a3912a610453f21de57
William Kemeny <williamkemeny@gmail.com>
2023-07-20 00:09:50 +0000

    Created the sql seeds, please review and let me know if there are any mistakes there are 20 users. Only users 1-3 made quizzes. Quizzes 1 and 2 have 4 questions with 4 responses. Quiz 3 is one quiz with 1 question. Quiz 4 is private and has 2 questions that are true/false. about 60 attempts

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,21 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@yahoo,ca','password');
+INSERT INTO users (name, email, password) VALUES ('Kira','kira@yahoo,ca','password');
+INSERT INTO users (name, email, password) VALUES ('Bob', 'bob@gmail.com', 'password123');
+INSERT INTO users (name, email, password) VALUES ('John', 'john@hotmail.com', 'qwerty');
+INSERT INTO users (name, email, password) VALUES ('Emma', 'emma@gmail.com', 'abc123');
+INSERT INTO users (name, email, password) VALUES ('Michael', 'michael@yahoo.com', 'pass1234');
+INSERT INTO users (name, email, password) VALUES ('Sophia', 'sophia@hotmail.com', 'hello123');
+INSERT INTO users (name, email, password) VALUES ('Daniel', 'daniel@gmail.com', 'test456');
+INSERT INTO users (name, email, password) VALUES ('Olivia', 'olivia@yahoo.com', 'securepass');
+INSERT INTO users (name, email, password) VALUES ('William', 'william@gmail.com', '123abc');
+INSERT INTO users (name, email, password) VALUES ('Ava', 'ava@hotmail.com', 'password123');
+INSERT INTO users (name, email, password) VALUES ('Alexander', 'alexander@gmail.com', 'pass123');
+INSERT INTO users (name, email, password) VALUES ('Mia', 'mia@yahoo.com', 'mypass456');
+INSERT INTO users (name, email, password) VALUES ('James', 'james@hotmail.com', 'testpass');
+INSERT INTO users (name, email, password) VALUES ('Charlotte', 'charlotte@gmail.com', 'hello456');
+INSERT INTO users (name, email, password) VALUES ('Benjamin', 'benjamin@yahoo.com', 'abc123pass');
+INSERT INTO users (name, email, password) VALUES ('Amelia', 'amelia@hotmail.com', 'passwordtest');
+INSERT INTO users (name, email, password) VALUES ('Elijah', 'elijah@gmail.com', 'testpass123');
+INSERT INTO users (name, email, password) VALUES ('Abigail', 'abigail@yahoo.com', 'secure456');
+INSERT INTO users (name, email, password) VALUES ('Ryan', 'ryan@hotmail.com', 'mypassword');
