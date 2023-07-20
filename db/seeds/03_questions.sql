e693b5c24bd4f5effff5ced8f6d3c099c83e6b36
William Kemeny <williamkemeny@gmail.com>
2023-07-20 20:47:43 +0000

    Fixed answer_id for the last 2 questions

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -8,5 +8,5 @@ INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, "question
 INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, "question 3",'eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 25);
 INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, "question 4",'eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 30);
 INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (3, "Written Agreement",'You will complete the assignment by friday the 20th', 33);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (4, "This is a private test",'eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 30);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (4, "Are you the owner",'You will complete the assignment by friday the 20th', 33);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (4, "This is a private test",'eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 35);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (4, "Are you the owner",'You will complete the assignment by friday the 20th', 37);
