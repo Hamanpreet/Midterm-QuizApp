61f8bac12060d35254cb3014e1928ead4c0f53fc
William Kemeny <williamkemeny@gmail.com>
2023-07-21 18:12:21 -0400

    added module.exports = router; to quizzes.js since it was not loading

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,17 +1,18 @@
+const express = require("express");
+const router = express.Router();
 
-const express = require('express');
-const router  = express.Router();
+const database = require("../db/database");
 
-const database = require("../db/database")
-
-
-router.get('/', (req, res) => {
-  database.getAllPublicQuizzes()
-  .then((quizzes)=> {
-    const templateVars = { quizzes: quizzes };
-    res.render("quizzes", templateVars)
-  })
-  .catch((err)=>{
-    console.log(`An error occurred: ${err}`)
-  });
+router.get("/", (req, res) => {
+  database
+    .getAllPublicQuizzes()
+    .then((quizzes) => {
+      const templateVars = { quizzes: quizzes };
+      res.render("quizzes", templateVars);
+    })
+    .catch((err) => {
+      console.log(`An error occurred: ${err}`);
+    });
 });
+
+module.exports = router;
