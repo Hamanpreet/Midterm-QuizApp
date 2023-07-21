f6ad3970e2752657399791b12f4cf01f0b3a8635
Hamanpreet <haman42626@gmail.com>
2023-07-20 21:51:56 -0400

    Add validation checks before register

--- db/schema/03_questions.sql
+++ db/schema/03_questions.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS questions CASCADE;
 
+
 CREATE TABLE questions (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   question VARCHAR(1000) NOT NULL,
-  answer_id INTEGER REFERENCES options(id)
+  answer_id INTEGER
 );
