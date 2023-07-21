440952076e9dc24d7e6d6dafead92ae2458e271a
William Kemeny <williamkemeny@gmail.com>
2023-07-21 13:21:15 +0000

    started the quiz settings feature

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const userApiRoutes = require("./routes/users-api");
 const usersRoutes = require("./routes/users");
 const login = require("./routes/login");
 const register = require("./routes/register");
-
+const quizSettings = require("./routes/quiz-settings");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
@@ -55,6 +55,7 @@ app.use("/users", usersRoutes);
 // Note: mount other resources here, using the same pattern above
 app.use("/login", login);
 app.use("/register", register);
+app.use("/quiz-settings", quizSettings);
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
