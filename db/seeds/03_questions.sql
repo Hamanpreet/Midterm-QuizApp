9f28cc9e0875818fd875827f7fac85342ce0c59d
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-07-23 04:34:33 +0000

    more work on question view

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -1,12 +1,12 @@
 -- question table seeds here
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 1','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 2);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 2','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 5);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 3','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 11);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 4','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 13);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 1','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 19);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 2','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 23);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 3','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 25);
-INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 4','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 30);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 1','What country has the highest life expectancy? ', 2);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 2','Which language has the more native speakers?', 5);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 3','What is the most common surname in the United States?', 11);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (1, 'question 4','What disease commonly spread on pirate ships?', 13);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 1','Who was the Ancient Greek God of the Sun?', 19);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 2','What year was the United Nations established?', 23);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 3','Who has won the most total Academy Awards?', 25);
+INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (2, 'question 4','What artist has the most streams on Spotify?', 30);
 INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (3, 'Written Agreement','You will complete the assignment by friday the 20th', 33);
 INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (4, 'This is a private test','eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce', 35);
 INSERT INTO questions (quiz_id, title, question, answer_id) VALUES (4, 'Are you the owner','You will complete the assignment by friday the 20th', 37);
--- routes/quizzes.js
+++ routes/quizzes.js
@@ -15,18 +15,28 @@ router.get('/', (req, res) => {
   });
 });
 
-//show a particular quiz questions
-router.get("/:id", (req, res) => {
-  database.getQuestionsWithQuizId(req.params.id)
-  .then((questions)=>{
-    console.log(questions);
-    const templateVars = {questions : questions};
-    res.render("questions",templateVars);
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
+// show a particular quiz questions
+router.get("/:id", async (req, res) => {
+  try {
+    const questions = await database.getQuestionsWithQuizId(req.params.id);
+
+    // Fetch options for all questions concurrently using Promise.all
+    const optionsPromises = questions.map((question) => {
+      return database.getOptionsWithQuestionId(question.id);
+    });
+
+    // Wait for all options to be fetched before proceeding
+    const optionsResults = await Promise.all(optionsPromises);
+    const options = optionsResults.map((optionsResult) => optionsResult);
+    console.log(options);
+    const templateVars = { questions, options };
+    res.render("questions", templateVars);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
 
 
 module.exports = router;
